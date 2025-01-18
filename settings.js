42c51a0720ec31a7dc5bd612ec3a76e7b3668901
KingMilly15 <bcboy0015@gmail.com>
2025-01-18 10:18:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2347057344983"
+global.pairingNumber = "2347041216994"
 
 
 
@@ -27,9 +27,9 @@ global.autoBio = true
 global.antiSpam = true
 global.sign = "𓃵"
 // ☞ ➻ ➽ ☛ 〆 
-global.bank = "Opay"
-global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.bank = "Smartcash"
+global.bankname = "Peace Godfred"
+global.accnumber = "9025286907"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
